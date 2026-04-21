fbe44a0223f60a58e5311551970553c621873619
shashwat <gshashwatparashar@gmail.com>
2026-04-21 20:49:26 +0530

    UC2:Feet and Inches measurement equalit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
+package com.apps.quantitymeasurement;
+
 public class Main {
 
-    static class Feet {
+    // ================= FEET CLASS =================
+    public static class Feet {
         private final double value;
 
         public Feet(double value) {
@@ -21,11 +24,52 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) {
+    // ================= INCHES CLASS =================
+    public static class Inches {
+        private final double value;
+
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+
+            if (this == obj) return true;
+
+            if (obj == null) return false;
+
+            if (getClass() != obj.getClass()) return false;
+
+            Inches other = (Inches) obj;
+            return Double.compare(this.value, other.value) == 0;
+        }
+    }
+
+    // ================= FEET DEMO =================
+    public static void demonstrateFeetEquality() {
 
         Feet f1 = new Feet(1.0);
         Feet f2 = new Feet(1.0);
 
+        System.out.println("Input: 1.0 ft and 1.0 ft");
         System.out.println("Output: Equal (" + f1.equals(f2) + ")");
+    }
+
+    // ================= INCHES DEMO =================
+    public static void demonstrateInchesEquality() {
+
+        Inches i1 = new Inches(1.0);
+        Inches i2 = new Inches(1.0);
+
+        System.out.println("Input: 1.0 inch and 1.0 inch");
+        System.out.println("Output: Equal (" + i1.equals(i2) + ")");
+    }
+
+    // ================= MAIN =================
+    public static void main(String[] args) {
+
+        demonstrateFeetEquality();
+        demonstrateInchesEquality();
     }
 }
